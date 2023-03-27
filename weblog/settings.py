2deab15f9206a644d0ae9c5bcb5ba7a98a6bf742
Nilesh darji <nilesh.28.2003@gmail.com>
2023-03-27 15:47:50 +0530

    added: email verfication, about, contact

--- weblog/settings.py
+++ weblog/settings.py
@@ -167,7 +167,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'beabloggers@gmail.com'
-EMAIL_HOST_PASSWORD = 'xpil grtz kfgh onzg'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 DOMAIN='http://127.0.0.1:8000/'
